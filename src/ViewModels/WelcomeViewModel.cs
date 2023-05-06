0299440f08806c481d3e445423ef2d716e96cabf
creinis <115181527+creinis@users.noreply.github.com>
2023-05-06 14:02:45 +0200

    Add files via upload

---
+++ src/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace GP23.ViewModels
+{
+	public class WelcomeViewModel
+    {
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
