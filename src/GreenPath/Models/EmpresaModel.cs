d469b6a9027a92e6fe2d852ceb716055bd3b6bd1
alvarosoaress <alvarosoares01@gmail.com>
2023-05-16 14:38:49 -0300

    Type fix for empresa controller

--- src/GreenPath/Models/EmpresaModel.cs
+++ src/GreenPath/Models/EmpresaModel.cs
@@ -5,35 +5,35 @@ using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribut
 
 namespace GreenPath.Models
 {
-    [Table("Pessoa_Juri")]
-    public class EmpresaModel
-    {
-        [Column("pessoa_juri_id")] // coluna no BD
-        [Display(Name = "Id")] // muda o nome do input que o FOR estiver atribuido
-        [Key]
-        [AutoIncrement]
-        public int? Id { get; set; }
+	[Table("Pessoa_Juri")]
+	public class EmpresaModel
+	{
+		[Column("pessoa_juri_id")] // coluna no BD
+		[Display(Name = "Id")] // muda o nome do input que o FOR estiver atribuido
+		[Key]
+		[AutoIncrement]
+		public string? Id { get; set; }
 
-        [Column("pessoa_juri_razao")]
-        [Display(Name = "Razão Social")]
-        [Required(ErrorMessage = "Preencha Razão!")]
-        [MinLength(5, ErrorMessage = "Mínimo de 5 caracteres !")]
-        public string? Razao { get; set; }
+		[Column("pessoa_juri_razao")]
+		[Display(Name = "Razão Social")]
+		[Required(ErrorMessage = "Preencha Razão!")]
+		[MinLength(5, ErrorMessage = "Mínimo de 5 caracteres !")]
+		public string? Razao { get; set; }
 
-        [Column("pessoa_juri_cnpj")]
-        [Display(Name = "CNPJ")]
-        [Required(ErrorMessage = "Preencha CNPJ !")]
-        [MinLength(3, ErrorMessage = "Mínimo de 13 caracteres !")]
-        public string? Cnpj { get; set; }
+		[Column("pessoa_juri_cnpj")]
+		[Display(Name = "CNPJ")]
+		[Required(ErrorMessage = "Preencha CNPJ !")]
+		[MinLength(3, ErrorMessage = "Mínimo de 13 caracteres !")]
+		public string? Cnpj { get; set; }
 
-        [Column("pessoa_juri_area")]
-        [Display(Name = "Área")]
-        [Required(ErrorMessage = "Preencha Área !")]
-        [MinLength(3, ErrorMessage = "Mínimo de 2 caracteres !")]
-        public string? Area { get; set; }
+		[Column("pessoa_juri_area")]
+		[Display(Name = "Área")]
+		[Required(ErrorMessage = "Preencha Área !")]
+		[MinLength(3, ErrorMessage = "Mínimo de 2 caracteres !")]
+		public string? Area { get; set; }
 
-        [Column("pessoa_juri_desc")]
-        [Display(Name = "Descrição")]
-        public string? Desc { get; set; }
-    }
+		[Column("pessoa_juri_desc")]
+		[Display(Name = "Descrição")]
+		public string? Desc { get; set; }
+	}
 }
