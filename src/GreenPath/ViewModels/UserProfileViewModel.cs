5a48a66d5f490523b0dbec343e44dfaaa998fb64
creinis <115181527+creinis@users.noreply.github.com>
2023-06-12 21:47:36 +0200

    Update UserProfileViewModel.cs
    IFormFile em User profile view model

--- src/GreenPath/ViewModels/UserProfileViewModel.cs
+++ src/GreenPath/ViewModels/UserProfileViewModel.cs
@@ -6,4 +6,5 @@ public class UserProfileViewModel
     public EmpresaModel? EmpresaData { get; set; }
     public IQueryable<CertificacaoModel>? Certificates { get; set; }
     public IQueryable<VagasModel>? Vagas { get; set; }
+    public IFormFile? Image { get; set; }
 }
